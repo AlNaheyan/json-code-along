a6650820a613246c80ee56d75f35e5dc57dadacf
Al Naheyan <aln@seoscholars.org>
2022-08-04 10:35:26 -0400

    Summary

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 /** @TODO - Access the pokemon data from data.js */
 /**         and print to the console to check */
+console.log(pokemon);
+
+
+
+
 
 
 /** @TODO - Update the page data using a single pokemon */
@@ -11,6 +16,22 @@
  *          pokemon's base stats
  */
 
+let bulbasaur = pokemon[0];
+
+let nameHeading = document.querySelector('#poke_name');
+nameHeading.innerHTML = `#${bulbasaur.id} - ${bulbasaur.name}`
+
+let pokeImg = document.querySelector("#poke_img");
+pokeImg.src = bulbasaur.image[0];
+
+let types = document.querySelector("#poke_types");
+types.innerHTML = `${bulbasaur.type[0]}/ ${bulbasaur.type[1]} type`;
+
+let statDivs = document.querySelectorAll("#poke_stats div");
+for(let i = 0; i < statDivs.length; i++){
+    let stat = statDivs[i].id;
+    statDivs[i].style["width"] = `${bulbasaur.base[stat] * 4}px`;
+}
 
 /** @TODO - Assign #random_btn to pick a random starter on click */
 /**         and update the page data accordingly
@@ -21,3 +42,11 @@
  *          to the randomly-chosen pokemon
  *      4. Add an eventListener to #random_btn with our new function!
  */
+
+ 
+function randomPokemonOnClick() {
+    pokemon[Math.floor(Math.random()*pokemon.length)]
+}
+
+let randomBtn = document.querySelector("#random_btn");
+randomBtn.addEventListener("click", randomPokemonOnClick);
